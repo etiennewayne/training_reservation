527362507fc0fa737578566a1cea8a06e8c86569
etiennewayne <etiennewayne@gmail.com>
2022-05-25 16:48:52 +0800

    update repo

--- app/Http/Controllers/Administrator/AppointmentController.php
+++ app/Http/Controllers/Administrator/AppointmentController.php
@@ -21,17 +21,17 @@ class AppointmentController extends Controller
         return view('administrator.appointments');
     }
 
-    public function getAppointmentTypes(Request $req){
-        $sort = explode('.', $req->sort_by);
+    // public function getAppointmentTypes(Request $req){
+    //     $sort = explode('.', $req->sort_by);
 
-        $data = \DB::table('appointment_types as a')
-            ->join('offices as b', 'a.office_id', 'b.office_id')
-            ->where('a.appointment_type', 'like', $req->type . '%')
-            ->orderBy($sort[0], $sort[1])
-            ->paginate($req->perpage);
+    //     $data = \DB::table('appointment_types as a')
+    //         ->join('offices as b', 'a.office_id', 'b.office_id')
+    //         ->where('a.appointment_type', 'like', $req->type . '%')
+    //         ->orderBy($sort[0], $sort[1])
+    //         ->paginate($req->perpage);
 
-        return $data;
-    }
+    //     return $data;
+    // }
 
     public function show($id){
         return Appointment::find($id);
@@ -39,28 +39,37 @@ class AppointmentController extends Controller
 
 
     public function getAppointments(Request $req){
-        $data = Appointment::with(['user', 'training_center'])->paginate($req->per_page);
+        $data = Appointment::with(['user', 'training_center'])
+            ->where('ref_no', 'like', $req->reference . '%')
+            ->paginate($req->per_page);
         return $data;
     }
 
 
 
     public function update(Request $req, $id){
+       
         $req->validate([
             'app_date' => ['required'],
-            'app_time' => ['required'],
+            'app_time_from' => ['required'],
+            'app_time_to' => ['required'],
+
         ]);
 
         $date =  $req->app_date;
         $ndate = date("Y-m-d", strtotime($date)); //convert to date format UNIX
 
-        $time = $req->app_time;
-        $ntime = date('H:i:s',strtotime($time)); //convert to format time UNIX
+        $timeFrom = $req->app_time_from;
+        $timeTo = $req->app_time_to;
+
+        $ntimeFrom = date('H:i:s',strtotime($timeFrom)); //convert to format time UNIX
+        $ntimeTo = date('H:i:s',strtotime($timeTo)); //convert to format time UNIX
 
         $data = Appointment::find($id);
         $data->training_center_id  = $req->training_center;
         $data->app_date  = $ndate;
-        $data->app_time = $ntime;
+        $data->app_time_from = $ntimeFrom;
+        $data->app_time_to = $ntimeTo;
         $data->remarks = $req->remarks;
         $data->save();
 
@@ -68,8 +77,6 @@ class AppointmentController extends Controller
             'status' => 'updated'
         ],200);
     }
-
-
 
 
 
